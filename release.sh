abe63a0d56a7241c0f874c9a3352b6f6f395ab2b
Ivan Velickovic <i.velickovic@unsw.edu.au>
2025-09-20 16:48:53 +1000

    release.sh fix
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- release.sh
+++ release.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 if [ "$#" -ne 1 ]; then
     echo "usage: ./release.sh VERSION"
     exit 1
 fi
 
+VERSION=$1
+
+nix build .
+
 git tag $VERSION
 
 git push origin tag $VERSION
 
-gh release create $VERSION release/*.tar.gz
+gh release create $VERSION result/*.tar.gz
 
